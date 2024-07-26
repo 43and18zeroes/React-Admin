5f48c82350a301826d6a25effc43986a6444ad67
Christoph <c.wagner.germany@gmail.com>
2024-07-26 17:01:39 +0200

    theme4

--- src/theme.js
+++ src/theme.js
@@ -124,7 +124,7 @@ export const tokens = mode => ({
 
 // mui theme settings
 export const themeSettings = mode => {
-  const color = tokens(mode);
+  const colors = tokens(mode);
 
   return {
     palette: {
@@ -163,5 +163,33 @@ export const themeSettings = mode => {
             },
           }),
     },
+    typography: {
+        fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+        fontSize: 12,
+        h1: {
+            fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+            fontSize: 40,
+        },
+        h2: {
+            fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+            fontSize: 32,
+        },
+        h3: {
+            fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+            fontSize: 24,
+        },
+        h4: {
+            fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+            fontSize: 20,
+        },
+        h5: {
+            fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+            fontSize: 16,
+        },
+        h6: {
+            fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+            fontSize: 14,
+        },
+    }
   };
 };
