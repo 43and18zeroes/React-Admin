1a6e5054d2f4736e33f1f76bb39dd927d8b092d6
Christoph <c.wagner.germany@gmail.com>
2024-07-29 15:59:22 +0200

    Topbar2

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,6 +1,13 @@
 import { Box, IconButton, useTheme } from '@mui/material';
 import { useContext } from 'react';
 import { ColorModeContext, tokens } from '../../theme';
+import InputBase from '@mui/material';
+import { LightModeOutlinedIcon } from '@mui/icons-material/LightModeOutlined';
+import { DarkModeOutlinedIcon } from '@mui/icons-material/LightModeOutlined';
+import { NotificationsOutlinedIcon } from '@mui/icons-material/LightModeOutlined';
+import { SettingOutlinedIcon } from '@mui/icons-material/LightModeOutlined';
+import { PersonOutlinedIcon } from '@mui/icons-material/LightModeOutlined';
+import { SearchIcon } from '@mui/icons-material/LightModeOutlined';
 
 const Topbar = () => {
   return <div>Topbar</div>;
