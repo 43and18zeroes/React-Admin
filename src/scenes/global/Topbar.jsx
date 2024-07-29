5f06ef2976d1bab7403d923991d3c6a5817ef376
Christoph <c.wagner.germany@gmail.com>
2024-07-29 17:34:24 +0200

    Topbar7

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,15 +1,13 @@
 import { Box, IconButton, useTheme } from '@mui/material';
 import { useContext } from 'react';
 import { ColorModeContext, tokens } from '../../theme';
-import InputBase from '@mui/material';
-import { LightModeOutlinedIcon } from '@mui/icons-material/LightModeOutlined';
-import { DarkModeOutlinedIcon } from '@mui/icons-material/DarkModeOutlinedIcon';
-import { NotificationsOutlinedIcon } from '@mui/icons-material/NotificationsOutlinedIcon';
-import { SettingOutlinedIcon } from '@mui/icons-material/SettingOutlinedIcon';
-import { PersonOutlinedIcon } from '@mui/icons-material/PersonOutlinedIcon';
-import { SearchIcon } from '@mui/icons-material/Search';
-
-const styledBox = styled(Box)``;
+import InputBase from '@mui/material/InputBase';
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import SearchIcon from '@mui/icons-material/Search';
 
 const Topbar = () => {
   const theme = useTheme();
@@ -42,7 +40,7 @@ const Topbar = () => {
         <NotificationsOutlinedIcon />
       </IconButton>
       <IconButton>
-        <SettingOutlinedIcon />
+        <SettingsOutlinedIcon />
       </IconButton>
       <IconButton>
         <PersonOutlinedIcon />
