89986db7554770395d9d1e6d3283eca1031aee85
Christoph <c.wagner.germany@gmail.com>
2024-07-29 15:54:57 +0200

    Topbar

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,5 +1,9 @@
+import { Box, IconButton, useTheme } from '@mui/material';
+import { useContext } from 'react';
+import { ColorModeContext, tokens } from '../../theme';
+
 const Topbar = () => {
-    return <div>Topbar</div>
-}
+  return <div>Topbar</div>;
+};
 
 export default Topbar;
