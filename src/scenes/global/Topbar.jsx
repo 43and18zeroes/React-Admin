8c9ba45dee60176fcfd9b63d92c814b224413b12
Christoph <c.wagner.germany@gmail.com>
2024-07-29 16:20:20 +0200

    Topbar3

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -3,14 +3,37 @@ import { useContext } from 'react';
 import { ColorModeContext, tokens } from '../../theme';
 import InputBase from '@mui/material';
 import { LightModeOutlinedIcon } from '@mui/icons-material/LightModeOutlined';
-import { DarkModeOutlinedIcon } from '@mui/icons-material/LightModeOutlined';
-import { NotificationsOutlinedIcon } from '@mui/icons-material/LightModeOutlined';
-import { SettingOutlinedIcon } from '@mui/icons-material/LightModeOutlined';
-import { PersonOutlinedIcon } from '@mui/icons-material/LightModeOutlined';
-import { SearchIcon } from '@mui/icons-material/LightModeOutlined';
+import { DarkModeOutlinedIcon } from '@mui/icons-material/DarkModeOutlinedIcon';
+import { NotificationsOutlinedIcon } from '@mui/icons-material/NotificationsOutlinedIcon';
+import { SettingOutlinedIcon } from '@mui/icons-material/SettingOutlinedIcon';
+import { PersonOutlinedIcon } from '@mui/icons-material/PersonOutlinedIcon';
+import { SearchIcon } from '@mui/icons-material/Search';
+
+const styledBox = styled(Box)``;
 
 const Topbar = () => {
-  return <div>Topbar</div>;
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+
+  return (
+    <Box display="flex" justifyContent="space-between" p={2}>
+      {/* SEARCH BAR */}
+      <Box
+        display="flex"
+        backgroundColor={colors.primary[400]}
+        borderRadius="3px"
+      >
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+        <IconButton>
+            <SearchIcon />
+        </IconButton>
+      </Box>
+
+      {/* ICONS */}
+      
+    </Box>
+  );
 };
 
 export default Topbar;
